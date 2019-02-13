7560b1720fe3b8634033989cf4542d1efe3d5761
bhagirathgudi <bhagirath009@hotmail.com>
2019-02-12 23:05:11 -0600

    Initial commit

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+	public partial class MainPage : ContentPage
+	{
+		private string text;
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			if (text == null) {
+				DisplayAlert("Please enter the text first","", "Ok");
+			} else DisplayAlert("Hola, you entered", text, "Ok");
+		}
+
+		private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Editor editor = (Editor)sender;
+			text = editor.Text;
+		}
+	}
+}
